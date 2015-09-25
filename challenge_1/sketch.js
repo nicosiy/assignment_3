9743943d6a9e37e166e1131f55d2bf8dd88dab9d
nicosiy <nicosiysoo@gmail.com>
2015-09-24 21:55:33 -0400

    challenge 1 sketch update

--- challenge_1/sketch.js
+++ challenge_1/sketch.js
@@ -1,18 +1,63 @@
 function setup() {
-	// create a place to draw
-	createCanvas(640, 360);
+	// Canvas Setup
+	createCanvas(640, 400);
 	noStroke();
 	noLoop();
 }
 
 function draw() {
-	// clear the background
-	background(50, 50, 150);
+	// Background Color
+	background(53, 53, 53);
 
-	// set a fill color
-	fill(255, 255, 255);
+	//Color Swatches:
 
-	// draw the ellipse
-	var diameter = random(100, 200);
-	ellipse(320, 180, diameter, diameter);
+	fill(0, 0, 0);
+	stroke(color(255, 255, 255));
+	strokeWeight(1);
+	rect(10, 160, 50, 50);
+
+	fill(29, -0, 0);
+	stroke(color(255, 255, 255));
+	strokeWeight(1);
+	rect(73, 160, 50, 50);
+
+	fill(52, 0, 0);
+	stroke(color(255, 255, 255));
+	strokeWeight(1);
+	rect(136, 160, 50, 50);
+
+	fill(74, 0, 0);
+	stroke(color(255, 255, 255));
+	strokeWeight(1);
+	rect(199, 160, 50, 50);
+
+	fill(98, 0, 0);
+	stroke(color(255, 255, 255));
+	strokeWeight(1);
+	rect(262, 160, 50, 50);
+
+	fill(122, 0, 0);
+	stroke(color(255, 255, 255));
+	strokeWeight(1);
+	rect(325, 160, 50, 50);
+
+	fill(146, 0, 0);
+	stroke(color(255, 255, 255));
+	strokeWeight(1);
+	rect(388, 160, 50, 50);
+
+	fill(171, 0, 0);
+	stroke(color(255, 255, 255));
+	strokeWeight(1);
+	rect(451, 160, 50, 50);
+
+	fill(196, 0, 0);
+	stroke(color(255, 255, 255));
+	strokeWeight(1);
+	rect(514, 160, 50, 50);
+
+	fill(221, 0, 0);
+	stroke(color(255, 255, 255));
+	strokeWeight(1);
+	rect(577, 160, 50, 50);
 }
