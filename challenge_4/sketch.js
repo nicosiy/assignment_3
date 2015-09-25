4e5be24a06866388c67fa6da7c11811c533941ab
nicosiy <nicosiysoo@gmail.com>
2015-09-24 23:02:53 -0400

    Chall 4&5 changes

--- challenge_4/sketch.js
+++ challenge_4/sketch.js
@@ -1,18 +1,63 @@
+//Ball Variables:
+
+var ballX = 200;
+var ballY = 100;
+var ballSpeedX = 5;
+var ballSpeedY = 5;
+var ball1 = 100;
+var ball2 = 200;
+var ballSpeed1 = 5;
+var ballSpeed2 = 5;
+
 function setup() {
-	// create a place to draw
-	createCanvas(640, 360);
-	noStroke();
-	noLoop();
+	createCanvas(640, 700);
 }
 
 function draw() {
-	// clear the background
-	background(150, 50, 50);
 
-	// set a fill color
-	fill(255, 255, 255);
+	// Move balls
+	ballX = ballX + ballSpeedX;
+	ballY = ballY + ballSpeedY;
+	ball1 = ball1 + ballSpeed1;
+	ball2 = ball2 + ballSpeed2;
 
-	// draw the ellipse
-	var diameter = random(100, 200);
-	ellipse(320, 180, diameter, diameter);
+	// right
+	if (ballX > width) {
+		ballSpeedX = -ballSpeedX;
+	}
+	if (ball1 > width) {
+		ballSpeed1 = -ballSpeed1;
+	}
+
+	// bottom
+	if (ballY > height) {
+		ballSpeedY = -ballSpeedY;
+	}
+	if (ball2 > height) {
+		ballSpeed2 = -ballSpeed2;
+	}
+
+	// left
+	if (ballX < 0) {
+		ballSpeedX = -ballSpeedX;
+	}
+	if (ball1 < 0) {
+		ballSpeed1 = -ballSpeed1;
+	}
+
+	// top
+	if (ballY < 0) {
+		ballSpeedY = -ballSpeedY;
+	}
+	if (ball2 < 0) {
+		ballSpeed2 = -ballSpeed2;
+	}
+
+	//Draw Balls
+
+	background(0);
+	ellipse(ballX, ballY, 50, 50);
+	ellipse(ball1, ball2, 50, 50);
+
+
 }
--- challenge_5/sketch.js
+++ challenge_5/sketch.js
@@ -1,18 +1,17 @@
 function setup() {
-	// create a place to draw
-	createCanvas(640, 360);
-	noStroke();
-	noLoop();
+	//Canvas setup
+	createCanvas(640, 400);
 }
 
 function draw() {
-	// clear the background
-	background(150, 150, 50);
 
-	// set a fill color
+	//BG color
+	background(0);
 	fill(255, 255, 255);
 
-	// draw the ellipse
-	var diameter = random(100, 200);
-	ellipse(320, 180, diameter, diameter);
+	//Circle Mouse
+	ellipse(mouseX, mouseY, 50, 50);
+
+
+
 }
